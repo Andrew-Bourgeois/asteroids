8361cbe674381b61903f84e41401f803b77186f6
Andrew-Bourgeois <xperiment626stitch@gmail.com>
2024-12-28 22:22:48 -0500

    create player class, update circleshape, update main, update constants

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -10,10 +11,32 @@ def main():
     # create new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # restrict game draw to 60 fps
+    clock = pygame.time.Clock()
+    dt = 0
+
     # create an infinite loop
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        # fill screen with black
         screen.fill("black")
+
+        # instantiate player
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        player = Player(x, y)
+        player.draw(screen)
+
+        # flip the screen (print to screen)
         pygame.display.flip()
+
+        # call the tick() method to wait 1/60th of a second
+        # store returned time in milliseconds to dt
+        dt = clock.tick(60) / 1000
+
 
     # print("Starting asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
---
+++ player.py
@@ -0,0 +1,49 @@
+from circleshape import *
+from constants import *
+
+# Player class
+class Player(CircleShape):
+    def __init__(self, x, y):
+        # parent class constructor
+        super().__init__(x, y, PLAYER_RADIUS)
+
+        # class specific properties
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        points = self.triangle()
+        pygame.draw.polygon(screen, "white", points, 2)
+        
+
+
+"""
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+"""
+    
